dadc65cd3782f1ca703e8fc25dd5fd0f859a520a
Mohamed HILAL-SHOKRY <mohamed.hilal-shokry@valeo.com>
2022-07-26 10:54:44 +0200

    test formula

--- BackendApp/src/main/java/com/valeo/sslnextgen/logic/ManPowerBLImpl.java
+++ BackendApp/src/main/java/com/valeo/sslnextgen/logic/ManPowerBLImpl.java
@@ -1,6 +1,12 @@
 package com.valeo.sslnextgen.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.eva.base.acl.IPerimeterManager;
+import com.eva.base.dal.Filter;
+import com.eva.base.dal.Filter.Operator;
+import com.eva.base.dal.SimpleFilter;
 import com.valeo.sslnextgen.base.logic.ManPowerBLBaseImpl;
 import com.valeo.sslnextgen.model.ManPower;
 import com.valeo.sslnextgen.logic.ManPowerPerimeterImpl;
@@ -19,11 +25,12 @@ public class ManPowerBLImpl extends ManPowerBLBaseImpl<ManPower> implements IMan
 		return null;
 	}
 
+	@Override
 	public ManPower getByAll(String service,String site,String currency) {
 		List<Filter> filters = new ArrayList<>();
-		filters.add(new SimpleFilter("service", service, Operator.EQUAL));
-		filters.add(new SimpleFilter("site", site, Operator.EQUAL));
-		filters.add(new SimpleFilter("currency", currency, Operator.EQUAL));
+		filters.add(new SimpleFilter("service", service));
+		filters.add(new SimpleFilter("site", site));
+		filters.add(new SimpleFilter("currency", currency));
 		List<ManPower> getAllList= super.getAll(filters);
 		if(getAllList!=null && getAllList.size()>0)
 		{
