72891dfb6d6b740500cb34e775cdf77fc5583c68
Murugesan Sathiyamoorthy R <smurugesan@vanenburg.com>
2022-07-28 09:53:26 +0000

    Update Services.java

--- BackendApp/src/main/java/com/valeo/sslnextgen/model/Services.java
+++ BackendApp/src/main/java/com/valeo/sslnextgen/model/Services.java
@@ -1,7 +1,9 @@
 package com.valeo.sslnextgen.model;
 
 import com.valeo.sslnextgen.base.model.ServicesBase;
+import com.eva.base.annotations.Table;
 
+@Table(name="ServicesNew", keys={"sid"})
 public class Services extends ServicesBase {
 	
 
