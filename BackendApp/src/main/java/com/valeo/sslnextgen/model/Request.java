62cd2d68616fe170a2bcce260dd73e99164fa91a
Murugesan Sathiyamoorthy R <smurugesan@vanenburg.com>
2022-07-28 09:52:34 +0000

    Update Request.java

--- BackendApp/src/main/java/com/valeo/sslnextgen/model/Request.java
+++ BackendApp/src/main/java/com/valeo/sslnextgen/model/Request.java
@@ -1,7 +1,9 @@
 package com.valeo.sslnextgen.model;
 
 import com.valeo.sslnextgen.base.model.RequestBase;
+import com.eva.base.annotations.Table;
 
+@Table(name="RequestNew", keys={"sid"})
 public class Request extends RequestBase {
 	
 
