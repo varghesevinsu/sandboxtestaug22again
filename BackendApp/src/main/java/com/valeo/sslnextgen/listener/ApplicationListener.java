57483beabc2d4a485e20badb27b612cba1c9bf82
Murugesan Sathiyamoorthy R <smurugesan@vanenburg.com>
2022-07-28 13:48:07 +0000

    Update ApplicationListener.java

--- BackendApp/src/main/java/com/valeo/sslnextgen/listener/ApplicationListener.java
+++ BackendApp/src/main/java/com/valeo/sslnextgen/listener/ApplicationListener.java
@@ -1,4 +1,19 @@
 package com.valeo.sslnextgen.listener;
 
+import com.eva.base.mail.providers.EmailProviderFactory;
+import com.eva.base.mail.providers.SendGridEmailProvider;
+import com.eva.base.mail.providers.IEmailProvider.EmailProviderTypes;
+
 public class ApplicationListener extends BaseApplicationListener {
+    @Override
+	public void registerProvider() {
+		super.registerProvider();
+		EmailProviderFactory.registerProvider(EmailProviderTypes.SEND_GRID, new SendGridEmailProvider());
+	}
+	
+	@Override
+	public void initializeConfigurations() {
+		super.initializeConfigurations();
+		EmailProviderFactory.configureProviderProperties();
+	}
 }
